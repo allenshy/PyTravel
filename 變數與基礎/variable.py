9c1ed530fc7a285f28d805be4a042d164171b048
allenshy <zzzallen2009@gmail.com>
2025-04-09 23:44:56 +0800

    更多基礎運算

---
+++ 變數與基礎/variable.py
@@ -0,0 +1,40 @@
+# # 基礎變數運算
+# hourly_salary = 150
+# day_salary = hourly_salary * 8
+# week_salary = day_salary * 5
+
+# print(week_salary)
+
+
+# # 可以問Python保留字
+# help("keywords")
+
+
+# # 如果把內建函數拿去當變數用，同名函數就失效
+
+# # 餘數
+# x = 15 % 6
+
+# # 最大可以整除的數
+# y = 15 // 6
+
+# print(x)
+# print(y)
+
+# # 次方
+# x = 3**2
+# print(x)
+
+# # !!Python運算的優先順序
+# # 1.()
+# # 2.次方**
+# # 3. * / % //, 左到右
+# # 4. + -, 左到右
+
+
+# # Python可以同時賦值多個變數
+# x = y = z = 20
+# print(x, y, z)
+
+# x, y = 1, 2
+# print(x, y)
--- 變數與基礎數學/variable.py
+++
@@ -1,6 +0,0 @@
-# 基礎變數運算
-hourly_salary = 150
-day_salary = hourly_salary * 8
-week_salary = day_salary * 5
-
-print(week_salary)
